b4ea4cc94c0a3ea939c1e34ef25563ec5b415061
Erva Savaliya <25cs095@charusat.edu.in>
2026-06-24 11:43:58 +0530

    Delete cyberkavach directory

--- cyberkavach/config/app.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'name' => 'CyberKavach',
-    'env' => getenv('APP_ENV') ?: 'production',
-    'url' => getenv('APP_URL') ?: 'http://localhost',
-    'debug' => filter_var(getenv('APP_DEBUG') ?: false, FILTER_VALIDATE_BOOLEAN),
-];
